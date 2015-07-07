8750afde7079bb8ed542d354462506797b5b47f2
LanderU <lander.usategui@gmail.com>
2015-07-07 21:46:08 +0200

    Add script with connect to Snappy

---
+++ EjerciciosBasicos/script_usb0.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+continuar="si"
+if [ "`ifconfig usb0 2>/dev/null`" ]; then
+	while [ $continuar=="si" ]; do
+		sudo ifconfig usb0 192.168.7.1
+		sleep 5
+		if [ "`ifconfig usb0 2>/dev/null`"  ]; then
+			continuar="si"
+		else
+			echo "Has desconectado la BBB, salimos"
+			break
+		fi
+	done
+else
+	echo "¡Advertencia!"
+	echo "--------------"
+	echo "No has conectado la BBB"
+fi
